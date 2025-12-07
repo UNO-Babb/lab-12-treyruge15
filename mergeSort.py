c2a232d9a80e44eba229128fab4a2f0fd87408a9
treyruge15 <treyruge15@gmail.com>
2025-12-07 23:11:04 +0000

    Lab 12 - Trey Ruge

--- mergeSort.py
+++ mergeSort.py
@@ -16,7 +16,7 @@ def main():
   random.seed(2020) # This makes sure that the random list will be the same every time.
 
 
-  numberTerms = 10000
+  numberTerms = 5000
 
   orderedList = []
   reversedList = []
@@ -33,24 +33,24 @@ def main():
   print("Begin Sorting %d elements." % numberTerms)
 
   startTime = time.time()
-  AllSorts.bubbleSort(orderedList)
+  AllSorts.mergeSort(orderedList)
   endTime = time.time()
   elapsedTime = endTime - startTime
   print("Ordered list time: %.5f seconds" % elapsedTime)
 
   startTime = time.time()
-  AllSorts.bubbleSort(reversedList)
+  AllSorts.mergeSort(reversedList)
   endTime = time.time()
   elapsedTime = endTime - startTime
   print("Reversed list time: %.5f seconds" % elapsedTime)
 
   startTime = time.time()
-  AllSorts.bubbleSort(randomList)
+  AllSorts.mergeSort(randomList)
   endTime = time.time()
   elapsedTime = endTime - startTime
   print("Random list time: %.5f seconds" % elapsedTime)
 
-  print("Sorting Complete")
+  print("mergeSort -- Sorting Complete")
 
 
 if __name__ == '__main__':
